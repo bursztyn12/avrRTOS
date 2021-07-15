c972e20b63923f70859b80adf7eadad6f27e6959
bursztyn12 <mbursztynowski@gmail.com>
2021-07-15 21:00:20 +0200

    tmp102 update, master feaute

--- tmp102.c
+++ tmp102.c
@@ -7,26 +7,49 @@
 
 #include "tmp102.h"
 #include "twi.h"
+#include "kernel.h"
 
 struct tmp102 tmp102;
 
 uint16_t temp_v = 0;
 uint8_t c_reg = 0;
+uint8_t tmp102_status = TMP102_IDLE;
 
-uint8_t tmp102_init(uint8_t address){
-	if (address < 0x48 && address > 0x4B){
+uint8_t tmp102_init(uint8_t a0_conn){
+	if (tmp102_status == TMP102_BUSY){
+		task_block(TMP102_BLOCKED);
+	}
+	
+	if (a0_conn < GND && a0_conn > SCL){
 		return 1;
 	}
-	tmp102.address = address;
+	
+	if (a0_conn == GND){
+		tmp102.address = ADDRESS_GND;
+	}else if (a0_conn == VCC){
+		tmp102.address = ADDRESS_VCC;
+	}else if (a0_conn == SDA){
+		tmp102.address = ADDRESS_SDA;
+	}else{
+		tmp102.address = ADDRESS_CLK;
+	}
+	
 	return 0;
 }
 
-
-
 float tmp102_get_temp(){
+	if (tmp102_status == TMP102_BUSY){
+		task_block(TMP102_BLOCKED);
+	}
+	
 	c_reg = TEMPERATURE_REGISTER;
-	twi_setup(tmp102.address, &c_reg, tmp102.b_temp, 1, 2);
+	twi_setup(tmp102.address, &c_reg, tmp102.b_temp, 1, 2, MULTIPLE_BYTE_READ);
+
+	PORTA |= (1 << 6);
 	
 	temp_v = ((*tmp102.b_temp) << 4) | (*(tmp102.b_temp+1) >> 4);
+	
+	tmp102_status = TMP102_IDLE;
+	
 	return temp_v * CONST;
 }
